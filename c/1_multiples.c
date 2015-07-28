51a58c3ea44d835954bdba95d62a2da9feaea28c
Guilherme Coltro <guilherme.coltro@cazamba.com>
2015-07-28 12:32:53 -0300

    Adição dos arquivos em C

---
+++ c/1_multiples.c
@@ -0,0 +1,23 @@
+/**
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ *
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
+#include <stdio.h>
+#include <time.h>
+
+const int MAX = 1000;
+int multiples();
+
+int main(){
+	int begin = clock();
+	printf("Result: %d\nExecution Time: %dms\n", multiples(), (int) clock()-begin);
+	return 0;
+}
+
+int multiples(){
+	int sum = 0;
+	for(int i=0; i<MAX-1 ; i++ && ((i%3 == 0 || i%5 == 0) ? sum += i : sum ) );
+	return sum;
+}
---
+++ c/3_largest_prime.c
@@ -0,0 +1,38 @@
+/**
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ *
+ * What is the largest prime factor of the number 600851475143 ?
+ */
+
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+
+int largest(long int n);
+int isPrime(long int n);
+
+int main(){
+	int begin = clock();
+	long int n = 600851475143;
+	printf("Result: %d\nExecution Time: %dms\n", largest(n), (int) clock()-begin);
+	return 0;
+}
+
+int largest(long int n){
+	if(isPrime(n) == 1) return n;
+	long int root = (long int) sqrt(n);
+	long int largest = 1;
+	for( long int i=2; (i<=root) ; (( isPrime(i)==1 && n%i==0) ? ((largest=i) && (i++)) : i++) );
+	return largest;
+}
+
+/**
+* Return = 1 - Is Prime
+* Return = 0 - Isn't Prime
+*/
+int isPrime(long int n){
+	int result = 1;
+	long int v = (long int) sqrt(n);
+	for( long int i=2; (i<=v && result!=0) ; ((n%i== 0) ? (result = 0) : i++) );
+	return result;
+}
